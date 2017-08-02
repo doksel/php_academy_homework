3000086b97539b1adb3aea98dcbdf6d91a873001
Andriy Fedorchenko <doksel7@gmail.com>
2017-08-02 16:24:31 +0300

    commit

--- frontend/Javascript/Homework 17.07.2017/9.js
+++ frontend/Javascript/Homework 17.07.2017/9.js
@@ -1,5 +1,21 @@
-var Student = {
-    name: 'vasia',
-    surname: 'petrov'
-};
-Student.age = 23;
+function Counter() {
+    var counter = 0;
+    var step = 2;
+
+    return {
+        next: function() {
+            return counter += step;
+        },
+        reset: function() {
+            return counter = 0;
+        },
+        setCounterValue: function(value) {
+            counter = value;
+        },
+        back: function() {
+            return counter -= step;
+        },
+    };
+}
+
+var c1 = new Counter();
